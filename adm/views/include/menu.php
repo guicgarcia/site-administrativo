cc372d527e482bff4c7eab411b37a01569f9c817
Guilherme Chaves Garcia <guiilhermelitoral@hotmail.com>
2021-07-29 17:57:40 -0300

    Update menu.php

--- adm/views/include/menu.php
+++ adm/views/include/menu.php
@@ -6,7 +6,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Celke</a>
+            <a class="navbar-brand" href="#">Sistema</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
