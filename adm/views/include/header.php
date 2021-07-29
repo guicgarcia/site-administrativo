1375eab049ee0cc29a614261969c190b18bc6304
Guilherme Chaves Garcia <guiilhermelitoral@hotmail.com>
2021-07-29 17:57:08 -0300

    Update header.php

--- adm/views/include/header.php
+++ adm/views/include/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Celke - Administrativo</title>
+        <title>Administrativo</title>
         <link href="<?php echo URL; ?>assets/css/bootstrap.css" rel="stylesheet">
         <link href="<?php echo URL; ?>assets/css/personalizado.css" rel="stylesheet">
         <link rel="icon" href="<?php echo URL; ?>assets/imagens/adm/favicon.ico">
